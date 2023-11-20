d0cc7a56be939dc630d41f3dc4b45500547900db
Devansh <devanshpc7017@gmail.com>
2023-11-20 21:07:25 +0530

    uri hidden

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://foodcourtbackend-main-db-0d5124721c1:N3Mt9NCVjwUa9MmjgcQCp8A4w9qkGp@prod-us-central1-1.lfuy1.mongodb.net/foodcourtbackend-main-db-0d5124721c1"
+const mongoURI = process.env.URI;
 
 const connectToMongo = async()=>{
     await mongoose.connect(mongoURI)
